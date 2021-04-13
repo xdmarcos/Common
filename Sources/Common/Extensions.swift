26c31a14d974273a3f3aa0b00394ca1572ba0409
xdmarcos <xdmarcos@hotmail.com>
2021-04-13 13:47:43 +0100

    Add Bundle extension with decode function for json files

--- Sources/Common/Extensions.swift
+++ Sources/Common/Extensions.swift
@@ -70,3 +70,21 @@ public extension Date {
     return formatter.string(from: self)
   }
 }
+
+extension Bundle {
+  func decode<T: Decodable>(_ type: T.Type, from file: String) -> T {
+    guard let url = self.url(forResource: file, withExtension: nil) else {
+      fatalError("Failed to lacate \(file) in bundle.")
+    }
+
+    guard let data = try? Data(contentsOf: url) else {
+      fatalError("Failed to load \(file) from bundle.")
+    }
+
+    guard let loaded = try? JSONDecoder().decode(T.self, from: data) else {
+      fatalError("Failed to decode \(file) from bundle.")
+    }
+
+    return loaded
+  }
+}
